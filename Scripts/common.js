6d6750f8dd8855bcc15d63940680db367bbe63bb
Prashant Khandelwal <prashant@midnightprogrammer.net>
2016-01-21 18:58:12 +0530

    Implement paging for the feeds
    Bookmark icon visible on feed title hover.

---
+++ Scripts/common.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    var elem_feed_heading = $('.heading');
+    var elem_bookmark = $('.bookmark');
+
+    elem_feed_heading.mouseover(function (e) {
+        $(this)[0].childNodes[1].childNodes[3].style.display = 'inline';
+    });
+    elem_feed_heading.mouseleave(function (e) {
+        $(this)[0].childNodes[1].childNodes[3].style.display = 'none';
+    });
+
+    elem_bookmark.css('display', 'none');
+
+})();

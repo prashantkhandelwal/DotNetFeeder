fddcc09306220495bff8d4e8d919ad061e117637
Prashant Khandelwal <prashant@midnightprogrammer.net>
2016-05-12 13:27:21 +0530

    Added feeds page

--- src/App_Code/Constants.cs
+++ src/App_Code/Constants.cs
@@ -1,5 +1,6 @@
 ﻿public static class Constants
 {
+    public const string DATA_FEED = "~/App_Data/feeder.json";
     public const string DATA_STORE_GEN_SETTINGS = "~/App_Data/generalsettings.json";
     public const string DATA_STORE_STORAGE_SETTINGS = "~/App_Data/storagesettings.json";
 }
